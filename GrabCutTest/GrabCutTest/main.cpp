20294a4c9c35440f9a2bc92971b2f05c488d7616
Chanhee Lee <rucold@naver.com>
2017-07-07 18:14:16 +0900

    Modify Drawing Box for use to grabcut

--- GrabCutTest/GrabCutTest/main.cpp
+++ GrabCutTest/GrabCutTest/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv/cv.h>
+
+void mouseCallback(int event, int x, int y, int flag, void* param);
+
+cv::Rect rectangleBox;
+bool isDrawingBox = false;
+int userBoxCount = 0;
+
+void drawBox(IplImage* img, CvRect rectangle) {
+	cvRectangle(img, cvPoint(rectangle.x, rectangle.y),
+				cvPoint(rectangle.x + rectangle.width, rectangle.y + rectangle.height),
+				cvScalar(0xff, 0x00, 0x00));
+
+}
 
 int main()
 {
@@ -9,27 +24,44 @@ int main()
 	cv::namedWindow("Original Image");
 	cv::imshow("Original Image", image);
 
-	// 입력 영상에 대한 부분 전경/배경 레이블을 지정하는 방법
-	// 전경 객체 내부를 포함하는 내부 직사각형을 정의
-	cv::Rect rectangle(10, 0, 380, 256);
-	// 경계 직사각형 정의
-	// 직사각형 밖의 화소는 배경으로 레이블링
+	
+	rectangleBox = cvRect(-1, -1, 0, 0);
 
-	// 입력 영상과 자체 분할 영상 외에 cv::grabCut 함수를 호출할 때
-	// 이 알고리즘에 의해 만든 모델을 포함하는 두 행렬의 정의가 필요
+	// Mat -> IplImage* 형 변환
+	IplImage* imageCopy;
+	IplImage temp;
+	temp = image;
+	imageCopy = &temp;
+
+	IplImage* tempForCopy = cvCloneImage(imageCopy);
+
+	cvSetMouseCallback("Original Image", mouseCallback, (void*)imageCopy);
+	//cvReleaseImage(&imageCopy);
+
+	while (1) {
+		cvCopy(imageCopy, tempForCopy);
+		if (isDrawingBox) drawBox(tempForCopy, rectangleBox);
+		cvShowImage("Original Image", tempForCopy);
+
+		if (cvWaitKey(15) == 27) break;
+
+		if (userBoxCount > 0) break;
+	}
+	cv::Rect rectangle(rectangleBox.x, rectangleBox.y,rectangleBox.width, rectangleBox.height);
+	
+	//사각형 안그려진 이미지를 다시 로드
+	image = cv::imread("input.jpg");
 	cv::Mat result; // 분할 (4자기 가능한 값)
 	cv::Mat bgModel, fgModel; // 모델 (초기 사용)
 	cv::grabCut(image,    // 입력 영상
 		result,    // 분할 결과
 		rectangle,   // 전경을 포함하는 직사각형
 		bgModel, fgModel, // 모델
-		5,     // 반복 횟수
-		cv::GC_INIT_WITH_RECT); // 직사각형 사용
-								// cv::CC_INT_WITH_RECT 플래그를 이용한 경계 직사각형 모드를 사용하도록 지정
+		15,     // 반복 횟수
+		cv::GC_INIT_WITH_RECT); 
 
-								// cv::GC_PR_FGD 전경에 속할 수도 있는 화소(직사각형 내부의 화소 초기값)
-								// cv::GC_PR_FGD와 동일한 값을 갖는 화소를 추출해 분할한 이진 영상을 얻음
 	cv::compare(result, cv::GC_PR_FGD, result, cv::CMP_EQ);
+
 	// 전경일 가능성이 있는 화소를 마크한 것을 가져오기
 	cv::Mat foreground(image.size(), CV_8UC3, cv::Scalar(255, 255, 255));
 	// 결과 영상 생성
@@ -44,4 +76,36 @@ int main()
 	cv::waitKey(0);
 
 	return 0;
+}
+
+void mouseCallback(int event, int x, int y, int flags, void* param) {
+	IplImage* image = (IplImage*)param;
+
+	switch (event) {
+	case CV_EVENT_MOUSEMOVE: {if (isDrawingBox) {
+		rectangleBox.width = x - rectangleBox.x;
+		rectangleBox.height = y - rectangleBox.y;
+	}
+	}
+							 break;
+	case CV_EVENT_LBUTTONDOWN: {
+		isDrawingBox = true;
+		rectangleBox = cvRect(x, y, 0, 0);
+	}
+							   break;
+	case CV_EVENT_LBUTTONUP: {
+		isDrawingBox = false;
+		if (rectangleBox.width < 0) {
+			rectangleBox.x += rectangleBox.width;
+			rectangleBox.width *= -1;
+		}
+		if (rectangleBox.height < 0) {
+			rectangleBox.y += rectangleBox.height;
+			rectangleBox.height *= -1;
+		}
+		drawBox(image, rectangleBox);
+		userBoxCount++;
+	}
+							 break;
+	}
 }
