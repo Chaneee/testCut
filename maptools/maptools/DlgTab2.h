559473a81eb0e92a1e13df541e1311afb6d037f6
Chanhee Lee <rucold@naver.com>
2017-11-03 10:50:29 +0900

    add ignore file config

---
+++ maptools/maptools/DlgTab2.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+// CDlgTab2 대화 상자입니다.
+
+class CDlgTab2 : public CDialog
+{
+	DECLARE_DYNAMIC(CDlgTab2)
+
+public:
+	CDlgTab2(CWnd* pParent = NULL);   // 표준 생성자입니다.
+	virtual ~CDlgTab2();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG2 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLvnItemchangedList1(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnNMCustomdrawSlider1(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnNMCustomdrawSlider2(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnNMCustomdrawSlider3(NMHDR *pNMHDR, LRESULT *pResult);
+};
